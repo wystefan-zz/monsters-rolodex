64765e51bf3dbb29faaf6d5e6f606ef9bdf6677f
yangwu <yangwu@yangwus-MacBook-Pro.local>
2020-08-23 15:25:59 -0700

    use searchBox component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
+
 import './App.css';
 
 class App extends Component {
@@ -27,10 +29,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input type='search' placeholder='search mosters' 
-          onChange={e => {
-            this.setState({ searchField: e.target.value }) // setState is an asynchronous function
-            }}/>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={e => this.setState({
+            searchField: e.target.value
+          })}
+        />
         <CardList monsters={filteredMosters}>
         </CardList>
       </div>
