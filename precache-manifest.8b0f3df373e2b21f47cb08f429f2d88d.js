cf19664d515c27d64a20ca9f39a1c8d5a31771ec
yangwu <yangwu@yangwus-MacBook-Pro.local>
2020-08-28 23:31:05 -0700

    Updates

---
+++ precache-manifest.8b0f3df373e2b21f47cb08f429f2d88d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5c2d20f8962a7ef8946adf9852992e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7af0b1a9c2c4dede756c",
+    "url": "/monsters-rolodex/static/css/main.47b34e26.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af0b1a9c2c4dede756c",
+    "url": "/monsters-rolodex/static/js/main.995435e9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
